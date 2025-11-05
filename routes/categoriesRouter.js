d119a23c312c89a6152236a05f27b2600320010a
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-11-05 14:53:59 -0500

    Create /new post and get routes
    For displaying form and posting new category to DB

--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -3,8 +3,8 @@ const router = Router();
 const controller = require("../controllers/categoriesController");
 
 router.get("/", controller.getAllCategories);
-// router.get("/new",)
-// router.post("/new",)
+router.get("/new", controller.getCategoryForm);
+router.post("/new", controller.postNewCategory);
 router.get("/:id", controller.getCategoryById);
 
 module.exports = router;
