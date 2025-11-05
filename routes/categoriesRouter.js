62c24941bdb0d84cbc95c011de9ae511297d9d73
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-11-05 16:00:37 -0500

    Create routes for GET/POST categories/:id/edit

--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -5,6 +5,8 @@ const controller = require("../controllers/categoriesController");
 router.get("/", controller.getAllCategories);
 router.get("/new", controller.getCategoryForm);
 router.post("/new", controller.postNewCategory);
+router.get("/:id/edit", controller.getEditCategoryForm);
+router.post("/:id/edit", controller.postEditCategory);
 router.get("/:id", controller.getCategoryById);
 
 module.exports = router;
