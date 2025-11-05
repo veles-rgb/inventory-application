5c263e094606171b901cd2cb51d963101bee735d
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-11-05 16:00:05 -0500

    Create routes for GET/POST animals/:id/edit

--- routes/animalsRouter.js
+++ routes/animalsRouter.js
@@ -5,6 +5,8 @@ const controller = require("../controllers/animalsController");
 router.get("/", controller.getAllAnimals);
 router.get("/new", controller.getAnimalForm);
 router.post("/new", controller.postNewAnimal);
+router.get("/:id/edit", controller.getEditAnimalForm);
+router.post("/:id/edit", controller.postEditAnimal);
 router.get("/:id", controller.getAnimalById);
 
 module.exports = router;
