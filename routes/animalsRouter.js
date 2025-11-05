cd3b7ae9e6207aa2aadb015a9db68d61b72071bc
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-11-05 14:52:50 -0500

    Create /new post and get routes
    For displaying form and posting new animal to DB

--- routes/animalsRouter.js
+++ routes/animalsRouter.js
@@ -3,8 +3,8 @@ const router = Router();
 const controller = require("../controllers/animalsController");
 
 router.get("/", controller.getAllAnimals);
-// router.get("/new",);
-// router.post("/new",);
+router.get("/new", controller.getAnimalForm);
+router.post("/new", controller.postNewAnimal);
 router.get("/:id", controller.getAnimalById);
 
 module.exports = router;
