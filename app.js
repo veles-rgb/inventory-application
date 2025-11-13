26130d1ec515c606c1cc71f4cab2130ee137c6a5
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-11-13 17:59:52 -0500

    Add process.env.PORT for Railway

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use("/", indexRouter);
 app.use("/animals", animalsRouter);
 app.use("/categories", categoriesRouter);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, (error) => {
     if (error) {
         throw error;
